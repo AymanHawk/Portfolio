98d9178cc913b58eb90bcf6d071f621b1782c4ca
AymanHawk <ahaque2002@gmail.com>
2024-11-27 23:47:00 -0500

    feat: shadcn init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */const nextConfig = {
+  images: {
+    domains: ['aceternity.com', 'i.scdn.co', 'assets.aceternity.com', 'raw.githubusercontent.com', 'cdn.thestorygraph.com'],
+  },
+  // other configurations if any
 };
 
-export default nextConfig;
+export default nextConfig;  
